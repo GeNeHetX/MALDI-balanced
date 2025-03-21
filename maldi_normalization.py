0f5817fa4708ef797741f7586f01be7081d1f6c6
ahmed344 <ahmedsharaf483@gmail.com>
2025-03-21 16:39:00 +0000

    Better naming for the maldi normalization script

--- maldi_normalization.py
+++ maldi_normalization.py
@@ -1,19 +1,23 @@
-import numpy as np
 import pandas as pd
-
 from utils import maldi_normalize
 
+# Define the path and normalization method
+path = 'data/MALDI_IHC/correlations/'
+normalization_method = 'TIC'
+
 # Load the MSI data
-peaks = pd.read_pickle("data/MALDI_IHC/peaks.pkl")
-pixels = pd.read_pickle("data/MALDI_IHC/pixels.pkl")
+peaks = pd.read_pickle(f"{path}peaks.pkl")
+pixels = pd.read_pickle(f"{path}pixels.pkl")
 
 # Get the number of lames and densities
 lames = pixels['run'].unique()
 
-# Noramlize the peaks using PQN
+# Noramlize the peaks for each lame using the specified method
 peaks_norm = peaks.copy()
 for lame in lames:
-    peaks_norm[pixels['run'] == lame] = maldi_normalize(peaks[pixels['run'] == lame])
+    print(f"Normalizing {lame}")
+    peaks_norm[pixels['run'] == lame] = maldi_normalize(peaks=peaks[pixels['run'] == lame],
+                                                        method=normalization_method)
 
 # Save to a pickle file
-peaks_norm.to_pickle("data/MALDI_IHC/peaks_normalized.pkl")
+peaks_norm.to_pickle(f"{path}peaks_normalized_{normalization_method}.pkl")
