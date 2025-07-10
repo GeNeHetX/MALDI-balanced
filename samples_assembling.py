0ff5cabeb2e36592777daef2ad5c61362fd63b91
ahmed344 <ahmedsharaf483@gmail.com>
2025-07-10 17:16:01 +0000

    Assembling in lesion

--- samples_assembling.py
+++ samples_assembling.py
@@ -1,18 +1,43 @@
-import numpy as np
 import pandas as pd
 import os
+import gc
+from tqdm import tqdm
 
-path = 'data_external/'
-path_results = 'data/MALDI_IHC/correlations/'
+path = 'data_external/new_lames'
+path_results = 'data/MALDI_IHC/aligned_peaks'
 
-# Extract the lames
-lames = sorted(os.listdir(path))
+# Extract the slides
+slides = sorted(os.listdir(path))
 
-# Load and concatenate the datasets
-peaks = pd.concat([pd.read_feather(f"{path}{lame}/results/mse_peaks_ref_corr.feather").astype('float32') for lame in lames])
-pixels = pd.concat([pd.read_feather(f"{path}{lame}/results/mse_pixels.feather") for lame in lames])
+# Initialize empty dataframes to store the results
+peaks, pixels = pd.DataFrame(), pd.DataFrame()
 
-# Reset the index
+# Iterate over the slides to load and concatenate the datasets
+pbar = tqdm(slides, desc="Processing slides")
+for slide in pbar:
+    pbar.set_description(f"Processing slide {slide}")
+
+    # Load and concatenate the datasets
+    peaks_slide = pd.read_pickle(f"{path}/{slide}/results/peaks_aligned.pkl")
+    pixels_slide = pd.read_feather(f"{path}/{slide}/results/mse_pixels.feather")
+
+    # Exclude the pixels out of lesion
+    peaks_slide = peaks_slide[pixels_slide['Density_Lesion'] > 0.5]
+    pixels_slide = pixels_slide[pixels_slide['Density_Lesion'] > 0.5]
+
+    # Remove the defects
+    peaks_slide = peaks_slide[pixels_slide['Density_Defects'] < 0.1]
+    pixels_slide = pixels_slide[pixels_slide['Density_Defects'] < 0.1]
+
+    # Concatenate the dataframes
+    peaks = pd.concat([peaks, peaks_slide], axis=0)
+    pixels = pd.concat([pixels, pixels_slide], axis=0)
+
+    # Clear memory
+    del peaks_slide, pixels_slide
+    gc.collect()
+
+# Reset the index of the final dataframes
 peaks.reset_index(drop=True, inplace=True)
 pixels.reset_index(drop=True, inplace=True)
 
@@ -24,13 +49,13 @@ microdissections = pixels.filter(regex='Density_microdissection_')
 pixels = pixels.drop(columns=microdissections.columns)
 
 # Rename the columns to remove the _mse suffix
-microdissections.columns = microdissections.columns.str.replace('Density_microdissection_', '')
+microdissections.columns = microdissections.columns.astype(str).str.replace('Density_microdissection_', '')
 
 # For consistency, change the datatypes of the pixels and microdissection columns to float32
 pixels = pixels.astype({col: 'float32' for col in pixels.columns if pixels[col].dtype == 'float64'})
 microdissections = microdissections.astype('float32')
 
 # Save the dataframes as pickle files
-peaks.to_pickle(f'{path_results}peaks.pkl')
-pixels.to_pickle(f'{path_results}pixels.pkl')
-microdissections.to_pickle(f'{path_results}microdissections.pkl')
+peaks.to_pickle(f'{path_results}/peaks.pkl')
+pixels.to_pickle(f'{path_results}/pixels.pkl')
+microdissections.to_pickle(f'{path_results}/microdissections.pkl')
